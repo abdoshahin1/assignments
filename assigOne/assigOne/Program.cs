727e0efa330ea8a5a7b181b1f4e47e437eb8c441
Abdo shahin <abdoahmedwork52@gmail.com>
2025-09-21 23:55:07 +0300

    assignment one

---
+++ assigOne/assigOne/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace assigOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region q1
+            //Console.WriteLine("Enter a number: ");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"number is : {num}");
+            #endregion
+            #region q2
+            //Console.WriteLine("Enter a number: ");
+            //int num2 = int.Parse(Console.ReadLine()); // using parse when pass a non_arthmetic value the convert does not occur and there is no exception nether convert and tryparse they will through exception
+            //Console.WriteLine($"number is : {num2}");
+            #endregion
+            #region q3
+            //float x = 3.45f;
+            //    x = x + 1;
+            //Console.WriteLine(x); // the operation goes correctly
+            #endregion
+            #region q4
+            //string test = "hello world!";
+            //Console.WriteLine(test.Substring(0,5));
+            #endregion
+            #region q5
+            //int x = 0;
+            //int y = 1;
+            //y = x; // here the value will swap but not the real one the compilor take copy of x and pass it to y
+            //Console.WriteLine(y); 
+            #endregion
+            #region q6
+            //test ts;
+            //ts = new test();
+
+            //test st = new test() { x = 10, y = 30 };
+
+            //st = ts; //  here the value will swap but the real one the compilor take origin value of x and pass it to y so the value of y change never ever
+
+            //Console.WriteLine(st.x);
+            //Console.WriteLine(st.y);
+            #endregion
+            #region q7
+            //string test = "hello";
+            //string test2 = "world!";
+            //Console.WriteLine(test + " " + test2);
+            #endregion
+            #region q8
+            //Console.Write("enter time: ");
+            //int time = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("\n enter amount: ");
+            //int amount = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("\n enter rate: ");
+            //decimal rate = Convert.ToDecimal(Console.ReadLine());
+
+            //Console.Write($"\n interest is = {(amount * rate * time) / 100}");
+
+            #endregion
+            #region q9
+            //Console.Write("enter weight (KG): ");
+            //int weight = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("\n enter height (M): ");
+            //int height = Convert.ToInt32(Console.ReadLine());
+            //Console.Write($"\n the BMI is {weight / (height * height)}");
+            #endregion
+            #region q10
+            //Console.WriteLine("enter a degree: ");
+            //int degree = int.Parse(Console.ReadLine());
+            //string result = (degree <= 10) ? "just cold" : (degree >= 30) ? "just hot" : "just good";
+            //Console.WriteLine(result);
+            #endregion
+            #region q11
+            //Console.WriteLine("enter the date: ");
+            //DateTime date = DateTime.Parse(Console.ReadLine());
+            //Console.WriteLine($"the date format is: {date: dd , MM , yyyy}");
+            //Console.WriteLine($"the date format is: {date: dd / MM / yyyy}");
+            //Console.WriteLine($"the date format is: {date: dd - MM  yyyy}");
+            #endregion
+            #region q12
+            //c the event is on 06/14/2024
+            #endregion
+            #region q13
+            //f a value 1 will be assigned to d
+            #endregion
+            #region q14
+                 // d 6 1
+            #endregion
+            #region q15
+                // d 7 7 
+            #endregion
+        }
+    }
+}
