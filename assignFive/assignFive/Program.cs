c0efb8b28e4f44de418b2dbefab189bf5e8973ad
abdoshahin1 <abdoahmadm55@gmail.com>
2025-10-06 16:55:07 +0300

    OOP - G2 - Assignment 2

---
+++ assignFive/assignFive/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace assignFive
+{
+    //struct Person
+    //{
+    //    public string Name;
+    //    public int age;
+    //}
+    //struct Point
+    //{
+    //    public int X;
+    //    public int Y;
+    //}
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region q1
+            //Person[] people = new Person[3];
+            //for (int i = 0; i < people.Length; i++)
+            //{
+            //    Console.WriteLine("Eneter name and age: ");
+            //    people[i].Name = Console.ReadLine();
+            //    people[i].age = int.Parse(Console.ReadLine());
+            //}
+            //foreach (Person person in people)
+            //{
+            //    Console.Write("name is " + person.Name);
+            //    Console.WriteLine("\t age is " + person.age);
+            //}
+            #endregion
+            #region q2
+            //Point p1 = new Point();
+            //Point p2 = new Point();
+            //Console.WriteLine("enter point x1|y1: ");
+            //p1.X = int.Parse(Console.ReadLine());
+            //p1.Y = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter point x2|y2: ");
+            //p2.X = int.Parse(Console.ReadLine());
+            //p2.Y = int.Parse(Console.ReadLine());
+            //double distance = Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+            //Console.WriteLine($"the distance is {distance}");
+            #endregion
+            #region q3
+            //Person[] people = new Person[3];
+            //for (int i = 0; i < people.Length; i++)
+            //{
+            //    Console.WriteLine("Eneter name and age: ");
+            //    people[i].Name = Console.ReadLine();
+            //    people[i].age = int.Parse(Console.ReadLine());
+            //}
+            //int oldest = people[0].age;
+            //string name = " ";
+            //for (int i = 0; i < people.Length; i++)
+            //{
+            //    if (oldest < people[i].age)
+            //    {
+            //        oldest = people[i].age;
+            //        name = people[i].Name;
+            //    }
+            //}
+            //Console.WriteLine($"the oldest person is {name} and his age is {oldest}.");
+            #endregion
+            #region q4
+            //double width =0, height = 0;
+            //Console.WriteLine("enter the dimention of rectangel: ");
+            //width = double.Parse(Console.ReadLine());
+            //height = double.Parse(Console.ReadLine());
+            //Rectangle rect = new Rectangle(width, height);
+            //Console.WriteLine($"rectangel area is {rect.Area}.");
+            //Console.WriteLine("\nUpdating values...");
+            //rect.Width = 15;
+            //rect.Height = 8;
+            //Console.WriteLine($"rectangel area is {rect.Area}."); // or we can display all info ==> rect.DisplayInfo();
+            #endregion
+
+        }
+    }
+}
---
+++ assignFive/assignFive/Rectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignFive
+{
+    internal struct Rectangle
+    {
+        private double width;
+        private double height;
+
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value >= 0)
+                    width = value;
+                else
+                    Console.WriteLine("Error: Width cannot be negative!");
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value >= 0)
+                    height = value;
+                else
+                    Console.WriteLine("Error: Height cannot be negative!");
+            }
+        }
+        public double Area
+        {
+            get{ return width * height;  }
+        }
+        public Rectangle(double width, double height)
+        {
+            this.width = 0;
+            this.height = 0;
+            Width = width;
+            Height = height;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Rectangle Width: {Width}");
+            Console.WriteLine($"Rectangle Height: {Height}");
+            Console.WriteLine($"Rectangle Area: {Area}");
+        }
+    }
+}
